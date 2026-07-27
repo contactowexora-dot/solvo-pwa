856c87845e54680317e3f55c41e36c8eb03dba6b
contactowexora-dot <contacto.wexora@gmail.com>
2026-07-27 12:00:24 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
  *    Por eso la puerta de acceso es la única pantalla que exige estar en línea.
  */
 
-const VERSION = 'solvo-cascaron-v9.0.0';
+const VERSION = 'solvo-cascaron-v9.0.1';
 
 /* Todo con ./ para que funcione bajo el subdirectorio de GitHub Pages
    (usuario.github.io/repo/), donde la raíz absoluta «/» no es la de la app. */
